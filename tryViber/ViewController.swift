c92d1f561338ec3014ec23750b108026cbf96ac5
Tsungyu Yu <ytyubox@gmail.com>
2020-01-03 16:25:47 +0800

    Initial Commit

---
+++ tryViber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tryViber
+//
+//  Created by 游宗諭 on 2020/1/3.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
