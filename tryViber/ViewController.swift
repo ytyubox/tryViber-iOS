a600e254efeee0a7ad7e1130aafab77822843bd3
Tsungyu Yu <ytyubox@gmail.com>
2020-01-03 16:49:41 +0800

    apply viber

--- tryViber/ViewController.swift
+++ tryViber/ViewController.swift
@@ -5,16 +5,22 @@
 //  Created by 游宗諭 on 2020/1/3.
 //  Copyright © 2020 游宗諭. All rights reserved.
 //
-
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+	
+	var timer:Timer?
+	@IBAction func didTapButton(_ sender: UIButton) {
+		timer?.invalidate()
+		var count = 4
+		timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) {
+			count -= 1
+			if count == 0 {
+				$0.invalidate()
+				return
+			}
+			AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+		}
 	}
-
-
 }
-
